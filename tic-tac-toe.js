8f01725f0c40dff144589a11602c4330298b6861
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2023-10-30 21:55:49 -0700

    create event listeners for each button

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -10,3 +10,125 @@ const DOM = {
     cellNine: document.getElementById("cell-nine"),
     playerType : document.getElementById("type")
 }
+
+DOM.cellOne.addEventListener("click", () =>{
+    player = DOM.playerType.innerText 
+    switch (player){
+         case "x": 
+             DOM.cellOne.innerHTML = "x"
+             break;
+         case "o":
+             DOM.cellOne.innerHTML = "o"
+             break;
+    }
+    
+    playerChoices(1)
+    changePlayerType()
+ 
+ })
+ 
+ DOM.cellTwo.addEventListener("click", () => {
+     player = DOM.playerType.innerText 
+     switch (player){
+          case "x": 
+              DOM.cellTwo.innerHTML = "x"
+              break;
+          case "o":
+              DOM.cellTwo.innerHTML = "o"
+              break;
+     }
+     
+     playerChoices(2)
+     changePlayerType()
+ })
+ DOM.cellThree.addEventListener("click", () => {
+     player = DOM.playerType.innerText 
+     switch (player){
+          case "x": 
+              DOM.cellThree.innerHTML = "x"
+              break;
+          case "o":
+              DOM.cellThree.innerHTML = "o"
+              break;
+     }
+     playerChoices(3)
+     changePlayerType()
+ })
+ DOM.cellFour.addEventListener("click", () => {
+     player = DOM.playerType.innerText 
+     switch (player){
+          case "x": 
+              DOM.cellFour.innerHTML = "x"
+              break;
+          case "o":
+              DOM.cellFour.innerHTML = "o"
+              break;
+     }
+     playerChoices(4)
+     changePlayerType()
+ })
+ DOM.cellFive.addEventListener("click", () => {
+     player = DOM.playerType.innerText 
+     switch (player){
+          case "x": 
+              DOM.cellFive.innerHTML = "x"
+              break;
+          case "o":
+              DOM.cellFive.innerHTML = "o"
+              break;
+     }
+     playerChoices(5)
+     changePlayerType()
+ })
+ DOM.cellSix.addEventListener("click", () => {
+     player = DOM.playerType.innerText 
+     switch (player){
+          case "x": 
+              DOM.cellSix.innerHTML = "x"
+              break;
+          case "o":
+              DOM.cellSix.innerHTML = "o"
+              break;
+     }
+     playerChoices(6)
+     changePlayerType()
+ })
+ DOM.cellSeven.addEventListener("click", () => {
+     player = DOM.playerType.innerText 
+     switch (player){
+          case "x": 
+              DOM.cellSeven.innerHTML = "x"
+              break;
+          case "o":
+              DOM.cellSeven.innerHTML = "o"
+              break;
+     }
+     playerChoices(7)
+     changePlayerType()
+ })
+ DOM.cellEight.addEventListener("click", () => {
+     player = DOM.playerType.innerText 
+     switch (player){
+          case "x": 
+              DOM.cellEight.innerHTML = "x"
+              break;
+          case "o":
+              DOM.cellEight.innerHTML = "o"
+              break;
+     }
+     playerChoices(8)
+     changePlayerType()
+ })
+ DOM.cellNine.addEventListener("click", () => {
+     player = DOM.playerType.innerText 
+     switch (player){
+          case "x": 
+              DOM.cellNine.innerHTML = "x"
+              break;
+          case "o":
+              DOM.cellNine.innerHTML = "o"
+              break;
+     }
+     playerChoices(9)
+     changePlayerType()
+ })
