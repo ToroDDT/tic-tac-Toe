2b9612dca805583b5aadc6927714738087059457
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2023-10-30 21:52:09 -0700

    create selectors for each cell on gameboard

---
+++ tic-tac-toe.js
@@ -0,0 +1,12 @@
+const DOM = {
+    cellOne: document.getElementById("cellone"),
+    cellTwo: document.getElementById("cell-two"),
+    cellThree: document.getElementById("cell-three"),
+    cellFour: document.getElementById("cell-four"),
+    cellFive: document.getElementById("cell-five"),
+    cellSix: document.getElementById("cell-six"),
+    cellSeven: document.getElementById("cell-seven"),
+    cellEight: document.getElementById("cell-eight"),
+    cellNine: document.getElementById("cell-nine"),
+    playerType : document.getElementById("type")
+}
